a8fd6623e1f47f9ec67b8b1bc7549eb78c0024aa
14haven <wndus9887@gmail.com>
2021-03-25 17:04:52 +0900

    주연 - 장바구니 페이지 완.

--- herb/src/main/java/com/kh/herb/cart/model/service/CartService.java
+++ herb/src/main/java/com/kh/herb/cart/model/service/CartService.java
@@ -1,5 +1,21 @@
 package com.kh.herb.cart.model.service;
 
-public interface CartService {
+import java.util.List;
 
+import com.kh.herb.cart.model.vo.Cart;
+import com.kh.herb.cart.model.vo.CartList;
+
+public interface CartService {
+	
+	// 카트 담기
+	public int addCart(Cart cart) throws Exception;
+	
+	// 카트 리스트
+	public List<CartList> cartList(String userId) throws Exception;
+
+	// 카드 옵션 수정
+	public int updateCart(Cart cart) throws Exception;
+	
+	// 카트 삭제
+	public int deleteCart(Cart cart) throws Exception;
 }
--- herb/src/main/java/com/kh/herb/cart/model/vo/Cart.java
+++ herb/src/main/java/com/kh/herb/cart/model/vo/Cart.java
@@ -1,5 +1,72 @@
 package com.kh.herb.cart.model.vo;
 
 public class Cart {
+	
+	/*
+	CREATE TABLE CART (
+	CART_NUM	NUMBER	PRIMARY KEY,
+	CART_STOCK	NUMBER	NOT NULL,
+	CART_USER_ID	VARCHAR2(10)	NOT NULL,
+	CART_P_NUM	NUMBER	NOT NULL
+	);
+	 
+	*/
+	
+	private int cartNum; // 카트 번호
+	private int cartStock; // 상품 갯수
+	private String cartUserId; // 사용자 아이디
+	private int cartPnum; // 상품 번호
+	
+	public Cart() {}
+	
+	public Cart(int cartNum, int cartStock, String cartUserId, int cartPnum) {
+		super();
+		this.cartNum = cartNum;
+		this.cartStock = cartStock;
+		this.cartUserId = cartUserId;
+		this.cartPnum = cartPnum;
+	}
 
+
+	public int getCartNum() {
+		return cartNum;
+	}
+
+
+	public void setCartNum(int cartNum) {
+		this.cartNum = cartNum;
+	}
+
+
+	public int getCartStock() {
+		return cartStock;
+	}
+
+
+	public void setCartStock(int cartStock) {
+		this.cartStock = cartStock;
+	}
+
+
+	public String getCartUserId() {
+		return cartUserId;
+	}
+
+
+	public void setCartUserId(String cartUserId) {
+		this.cartUserId = cartUserId;
+	}
+
+
+	public int getCartPnum() {
+		return cartPnum;
+	}
+
+
+	public void setCartPnum(int cartPnum) {
+		this.cartPnum = cartPnum;
+	}
+	
+	
+	
 }
