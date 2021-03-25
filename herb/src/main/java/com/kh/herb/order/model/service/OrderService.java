3e124c129e2c490085652ad006263d6101873e9e
14haven <wndus9887@gmail.com>
2021-03-25 17:54:25 +0900

    주연 - 결제페이지 장바구니 리스트 불러오기

--- herb/src/main/java/com/kh/herb/order/model/service/OrderService.java
+++ herb/src/main/java/com/kh/herb/order/model/service/OrderService.java
@@ -1,5 +1,13 @@
 package com.kh.herb.order.model.service;
 
+import java.util.List;
+
+import com.kh.herb.cart.model.vo.CartList;
+
 public interface OrderService {
 
+	// 카트 리스트
+	public List<CartList> cartList(String userId) throws Exception;
+	
+	
 }
--- herb/src/main/java/com/kh/herb/order/model/service/OrderServiceImpl.java
+++ herb/src/main/java/com/kh/herb/order/model/service/OrderServiceImpl.java
@@ -1,5 +1,22 @@
 package com.kh.herb.order.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.herb.cart.model.vo.CartList;
+import com.kh.herb.order.model.dao.OrderDAO;
+
+@Service
 public class OrderServiceImpl implements OrderService {
 
+	@Autowired
+	OrderDAO orderDAO;
+	
+	// 카트 리스트
+	@Override
+	public List<CartList> cartList(String userId) throws Exception {
+		return orderDAO.cartList(userId);
+	}
 }
